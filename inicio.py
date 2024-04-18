9c2d5e08b65fa50e680bef5b66b37b646ce35a56
Sala F502 Tecnologico Comfenalco <f502@Academico.local>
2024-04-18 10:26:28 -0500

    Create inicio.py

---
+++ inicio.py
@@ -0,0 +1,12 @@
+x= 10
+
+nombre= input("coloque su nombre")
+password= input("coloque su contraseña")
+
+while password==0:
+    if password==0: 
+        print("bienvenido", nombre)
+        break
+    else:
+        print("coloque una contraseña valida")
+        
